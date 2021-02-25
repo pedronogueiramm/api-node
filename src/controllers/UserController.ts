e6fcbc2322f75b096c87e9e29a7e2f1a09cfcd0d
Pedro Moreno <pedronog195@gmail.com>
2021-02-24 21:13:46 -0300

    concluindo controler do user e adicionando controler/model do survey

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,6 @@
-import {Request, Response} from "express";
-import { getRepository, RepositoryNotTreeError } from "typeorm";
-import { User } from "../models/User";
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
 
@@ -8,7 +8,7 @@ class UserController {
 
         const {name, email} = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email,
@@ -31,4 +31,4 @@ class UserController {
     }
 }
 
-export {UserController};
+export { UserController };
---
+++ src/repositories/SurveysRepository.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Survey } from "../models/Survey";
+
+
+@EntityRepository(Survey)
+class SurveysRepository extends Repository<Survey>{
+
+
+
+}
+
+export { SurveysRepository };
+
